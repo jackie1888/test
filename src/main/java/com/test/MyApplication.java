52f1da1777a0b7910e88c62f85bc82e42c22b4a0
jackieweng163 <51231584+jackieweng163@users.noreply.github.com>
2022-02-27 10:24:49 +0800

    0227

--- src/main/java/com/test/MyApplication.java
+++ src/main/java/com/test/MyApplication.java
@@ -3,6 +3,6 @@ package com.test;
 public class MyApplication {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("test");
+        System.out.println("test0222werwqer");
     }
 }
